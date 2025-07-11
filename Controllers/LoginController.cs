5948ac0d117914b916506221caccb3edb3333f28
reane <act.ralconera@gmail.com>
2025-07-11 13:13:04 +0800

    login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using InfasLogin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InfasLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // Simulated user credentials (insecure)
+        private const string HardcodedUsername = "user";
+        private const string HardcodedPassword = "password";
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            if (user.Username == HardcodedUsername && user.Password == HardcodedPassword)
+            {
+                // Set session
+                HttpContext.Session.SetString("Username", user.Username);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Invalid username or password.";
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InfasLogin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
